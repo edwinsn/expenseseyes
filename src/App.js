047046032e5903845708fab74567651fc447ea1c
Edwin Sanchez <edwinsanchez750@gmail.com>
2021-01-22 14:59:04 -0500

    Problems with the y axis solved.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Bar from './Components/Bar'
 function App() {
   return (
     <div className="App">
-        <Bar heights={[30,60,70,100,80,100,80]} labels={["A", "B","C","D","F","G","H"]} />
+        <Bar heights={[10,30,20,60,70,100,80,100,80,200]} labels={["A", "B","C","D","F","G","H","J","Last One"]} />
     </div>
   );
 }
--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -5,60 +5,92 @@ import '../assets/css/bar.css'
 
 class Bar extends Component{
     
-    render(){
+render(){
 
-        let bars = [] ;
-        let frecuences = [];  
-        let labelsStyles=[];
+    let bars = [] ;
+    let frecuences = [];  
+   
+//Normalize(0,100) the data
 
-        let heightsStyle = this.props.heights.sort(function(a, b){return a - b}).map((h,i,heights)=>{
+//bars styles
 
-            labelsStyles[i]={marginTop:(!heights[i-1])?(-heights[i]):Math.abs(heights[i]-heights[1])}
-            console.log(labelsStyles[i])
-            return {height: h+"px",marginTop:Math.max(...this.props.heights)-h}
+    let labelsStyles=[];
+    let maxHeight = Math.max(...this.props.heights);
+
+    let heightsStyle = this.props.heights.sort(function(a, b){return a - b}).map((h,i,heights)=>{
+
+        labelsStyles[i]={marginTop:(!heights[i-1])?(-heights[i]):Math.abs(h-heights[1])}
+
+        labelsStyles[i]={marginTop:0};
+        return {height: h+"px",marginTop:maxHeight-h}
         
+    });
+
+    let barStyles = heightsStyle.map((height, index)=>{
+        return {...height,...this.props.widths[index],...this.props.backGroundColor[index]}
+    });
+
+    let right = 0;
+
+    barStyles.forEach((Style,index)=>{
+
+        let adjustLabel = 22;
+            console.log(right)
+        bars.push(<div key= {index} className="bar inline" style={Style}>
+                      <div className="horizontalLines" style={{width:right+"px"}}></div>
+                      <p className="label" style={{marginTop:(Style['height'].slice(0,-2)-adjustLabel)+"px"}} >{this.props.labels[index]}</p>
+                  </div>);
+        right +=this.props.heights.length*this.props.widths[0].width.slice(0,-2)*index+2;
+        console.log(right)
+
+    });
+
+
+// frecuences style (labels in the y-axe)
+
+let frecuencesfontSize={fontSize:maxHeight/13}
+let paddingTop = "19px"
+
+    for(let i=0;i<=maxHeight/40;i++){
+
+        paddingTop= (i===maxHeight/40)?0:paddingTop;
+
+        frecuences.unshift(
+            <div  style={{...frecuencesfontSize,paddingTop:paddingTop}}>{2*i}</div>
+        );
+    }
+
+/*
+
+    let uniqueHeights = this.props.heights.filter( (value, index, heights) =>{
+        return heights.indexOf(value) === index;
         });
 
-        let uniqueHeights = this.props.heights.filter( (value, index, heights) =>{
-            return heights.indexOf(value) === index;
-        });
 
-
-        let frecuenceFontHeight =Math.round( (Math.max(...this.props.heights) / uniqueHeights.length) -12 )
-        let frecuencesStyle = {
+    let frecuenceFontHeight =Math.round( maxHeight / (3*uniqueHeights.length)) 
+    let frecuencesfontSize = {
               fontSize: frecuenceFontHeight+"px"
-        };
+    };
 
 
-        uniqueHeights.forEach( (h,i,uniHeight)=>{
+    uniqueHeights.forEach( (h,i,uniHeights)=>{
 
-            let paddingTop = (uniHeight[i+1]?uniHeight[i+1]:h)-h ;
-            console.log(paddingTop)
-            paddingTop = ((paddingTop-frecuenceFontHeight)>0)?(paddingTop-9):0;
+        let paddingTop = (uniHeights[i+1]?uniHeights[i+1]:h)-h ;
 
-            
-            console.log(paddingTop)
-            console.log("----")
+        paddingTop = ((paddingTop-frecuenceFontHeight)>0)?(paddingTop-6):0;
 
-            frecuences.unshift(<div key={frecuencesStyle['paddingTop'] } style={{...frecuencesStyle,paddingTop:paddingTop}}>{h}</div>);
+        frecuences.unshift(
+            <div  style={{...frecuencesfontSize,paddingTop:paddingTop}}>{h+"-"}</div>);
+    });
 
-          });
+*/        
+    let chartWidth = this.props.heights.length*this.props.widths[0].width.slice(0,-2)+3*this.props.heights.length+45;
 
-        let barStyles = heightsStyle.map((height, index)=>{
-            return {...height,...this.props.widths[index],...this.props.backGroundColor[index]}
-        });
-        barStyles.forEach((Style,index)=>{
-
-            bars.push(<div key= {index} className="bar inline" style={Style}><p className="label" style={labelsStyles[index]}>{this.props.labels[index]}</p></div>);
-
-       
-        });     
-        
 
         return (
             <section>
-            <div className="frecuences inline" style={{height:(Math.max(...this.props.heights)-5)+"px"}} >{frecuences}</div>
-            <div className="chart inline ">
+            <div className="frecuences inline" style={{height:maxHeight+"px"}} >{frecuences}</div>
+            <div className="chart inline " style={{width:chartWidth+"px"}}>
                     {bars}
             </div>
             </section>
@@ -71,7 +103,7 @@ const defaulWidths = []
 const backGroundColor = []
 for(let i=0;i<10;i++)  defaultHeights.push("40px")
 for(let i=0;i<10;i++)  defaulWidths.push({width:"30px"})
-for(let i=0;i<10;i++)  backGroundColor.push({background:"aqua"})
+for(let i=0;i<10;i++)  backGroundColor.push({background:"rgb(0, 102, 255)"})
 
 
 
